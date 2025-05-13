f41322e85e50c0aa45792257e8feb44570ae6e33
Mati Joseph <crucialniccur@gmail.com>
2025-05-13 19:30:36 +0300

    initialize new students with a knowledge that points to an empty list

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,14 @@
 
 from user import User
 
+
 class Student(User):
-    
+
+    knowledge = []
+
+    def __init__(self, first_name, last_name, knowledge=knowledge):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+
     def learn(self):
         pass
